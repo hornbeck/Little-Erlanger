fc49b07274932c39688f1adb4894d51bdde5aa3f
Johnathon Hornbeck <hornbeck@alter.local>
2009-05-26 13:07:54 -0500

    finished draft of chapter two on modules and functions.  Updated little_erlanger.erl to include full module up to this point.  3,396 words

--- src/little_erlanger.erl
+++ src/little_erlanger.erl
@@ -1,5 +1,13 @@
 -module(little_erlanger).
--export([double/1]).
+-export([double/1, add/2, multimath/1]).
 
 double(X) ->
   X * 2.
+
+add(X, Y) ->
+  X + Y.
+
+multimath({add, X, Y}) ->
+  {add, X + Y};
+multimath({double, X}) ->
+  {double, X * 2}.
